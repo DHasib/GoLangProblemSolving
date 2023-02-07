49cb64cbbef4c6d5e2a55b2723494c2169c6785e
Md_Hasib <mdhasib522@gmail.com>
2023-02-07 20:25:56 +0600

    Sum of all the multiples of 3 or 5 below 1000

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
+//Find the sum of all the multiples of 3 or 5 below 1000.”
 func main() {
 	sum := 0
 	for v := 1; v < 1_000; v++ { //set range
-		if v%3 == 0 || v%5 == 0 {
-			sum += v
+		if v%3 == 0 || v%5 == 0 { //making condition to cheak 3 & 5 multiples
+			sum += v // store & addition 3 & 5 multiples 
 		}
 		
 	}
-	fmt.Printf("Sum: %d\n", sum)
+	fmt.Printf("Sum: %d\n", sum) //show 3 & 5 multiples result the sum
 }
