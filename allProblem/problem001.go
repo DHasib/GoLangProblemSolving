1d63030befbf2a797e484a15f57603af65a2550a
Md_Hasib <mdhasib522@gmail.com>
2023-02-12 15:47:56 +0600

    Filing the project and using a go module to create a package

---
+++ allProblem/problem001.go
@@ -0,0 +1,15 @@
+package problems
+
+import "fmt"
+
+//Find the sum of all the multiples of 3 or 5 below 1000
+func  SummationOfMultiplesValues() {
+	sum := 0
+	for v := 1; v < 1_000; v++ { //set range
+		if v%3 == 0 || v%5 == 0 { //making condition to cheak 3 & 5 multiples
+			sum += v // store & addition 3 & 5 multiples 
+		}
+		
+	}
+	fmt.Printf("Summation Of of 3|5 multiples: %d\n", sum) //show 3 & 5 multiples result the sum
+}
--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import "fmt"
+import (
+	p "problems/allProblem"// 1st path will be register module name & 2nd path is Folder name
+)
 
-//Find the sum of all the multiples of 3 or 5 below 1000.”
 func main() {
-	sum := 0
-	for v := 1; v < 1_000; v++ { //set range
-		if v%3 == 0 || v%5 == 0 { //making condition to cheak 3 & 5 multiples
-			sum += v // store & addition 3 & 5 multiples 
-		}
-		
-	}
-	fmt.Printf("Sum: %d\n", sum) //show 3 & 5 multiples result the sum
+	p.SummationOfMultiplesValues()
 }
